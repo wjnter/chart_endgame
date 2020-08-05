233c64f180dbc1ede8ad306dc70cfc9b721c8340
wjnter <tptdong97@gmail.com>
2020-08-05 20:27:53 +0700

    Updates

---
+++ precache-manifest.49b40f0975c1d59301c2a1b9d894db9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3025316bd9966b2f175c474e607fb15e",
+    "url": "/chart_endgame/index.html"
+  },
+  {
+    "revision": "91a55036217587da88bf",
+    "url": "/chart_endgame/static/css/2.e33194e6.chunk.css"
+  },
+  {
+    "revision": "4bf2f643fdcb3f5a4c68",
+    "url": "/chart_endgame/static/css/main.b7082373.chunk.css"
+  },
+  {
+    "revision": "91a55036217587da88bf",
+    "url": "/chart_endgame/static/js/2.a99aac8d.chunk.js"
+  },
+  {
+    "revision": "730ffd10f627bccad7f26349c756e4a4",
+    "url": "/chart_endgame/static/js/2.a99aac8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf2f643fdcb3f5a4c68",
+    "url": "/chart_endgame/static/js/main.ec74e0ec.chunk.js"
+  },
+  {
+    "revision": "5de2d79559d38f1784f5",
+    "url": "/chart_endgame/static/js/runtime-main.f7662ed5.js"
+  }
+]);
